538d078f4439e701534beda87fdabce2063f7c77
oleg voronin <oleg83voronin@gmail.com>
2025-06-15 15:16:57 +0300

    task1

--- home2.js
+++ home2.js
@@ -12,3 +12,69 @@
 // Реализуйте метод hasBook(title), который будет проверять наличие книги в библиотеке и возвращать true или false в зависимости от того, есть ли такая книга в списке или нет.
 
 // Реализуйте конструктор, который принимает начальный список книг (массив) в качестве аргумента. Убедитесь, что предоставленный массив не содержит дубликатов; в противном случае выбрасывайте ошибку.
+
+class Library {
+  #books = [];
+
+  constructor(books) {
+    let uniqueBooks = new Set(books);
+    if (uniqueBooks.size !== books.length) {
+      throw new Error("Массив книг содержит дубликаты!");
+    }
+
+    this.#books = Array.from(uniqueBooks);
+  }
+
+  addBook(title) {
+    if (this.#books.includes(title)) {
+      throw new Error("Книга с таким названием уже есть в библиотеке");
+    }
+    this.#books.push(title);
+    return this.#books;
+  }
+
+  removeBook(titleToRemove) {
+    if (!this.#books.includes(titleToRemove)) {
+      throw new Error("Книги с таким названием нет в библиотеке");
+    }
+
+    const index = this.#books.indexOf(titleToRemove); 
+    if (index !== -1) {
+      this.#books.splice(index, 1); 
+    }
+  }
+  
+  hasBook(title) {
+    if (this.#books.includes(title)) {
+      console.log(`Книга "${title}" есть в библиотеке`);
+    } else {
+      console.log(`Книги "${title}" нет в библиотеке`);
+    }
+  }
+ 
+  get allBooks() {
+    return this.#books;
+  }
+}
+//Проверка
+
+// const books0 = [  "Игра престолов", "Гарри Поттер", "Гарри Поттер", "Властелин колец"]; // Дубликат
+// const library0 = new Library(books0); //Ошибка: Массив книг содержит дубликаты!
+
+const books1 = ["Игра престолов", "Гарри Поттер", "Властелин колец"];
+const library1 = new Library(books1);
+console.log(library1.allBooks); // ['Игра престолов', 'Гарри Поттер', 'Властелин колец']
+
+library1.addBook("Преступление и наказание");
+console.log(library1.allBooks); // ['Игра престолов', 'Гарри Поттер', 'Властелин колец', 'Преступление и наказание']
+
+// library1.addBook("Преступление и наказание");
+// console.log(library1.allBooks); // Ошибка: Книга с таким названием уже есть в библиотеке
+
+library1.removeBook("Гарри Поттер");
+console.log(library1.allBooks); // ['Игра престолов', 'Властелин колец', 'Преступление и наказание']
+
+// library1.removeBook("Гарри Поттер");
+// console.log(library1.allBooks); // Ошибка: Книги с таким названием нет в библиотеке
+
+library1.hasBook("Преступление и наказание");
